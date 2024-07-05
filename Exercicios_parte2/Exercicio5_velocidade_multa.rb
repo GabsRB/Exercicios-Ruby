2dce6a7787d9ffcab7a3c55d71ba6a6d7f93b719
Gabrielle Barreto <105834363+GabsRB@users.noreply.github.com>
2024-07-05 15:06:41 -0300

    Add files via upload

--- Exercicios_parte2/Exercicio5_velocidade_multa.rb
+++ Exercicios_parte2/Exercicio5_velocidade_multa.rb
@@ -5,36 +5,37 @@ sabendo que são pagos: R$ 50 reais se o motorista ultrapassar em até 10km/h a 
 se o motorista ultrapassar de 11 a 30 km/h a velocidade permitida; e R$ 200 reais, se estiver acima de 31km/h da velocidade permitida.
 =end
 
+# Inicia com um loop infinito para continuar com outros valores, que só será encerrado com a condição de break
 loop do
-puts "Qual a velocidade permitida na avenida: "
-velPermitida = gets.chomp.to_i
-
-puts "Qual a velocidade do motorista: "
-velMoto = gets.chomp.to_i
-
-calMulta = velMoto - velPermitida 
-
-if(calMulta <= 10)
-        puts "Você estava na velocidade #{velMoto} Km/h em uma velocidade permitida na Avenida #{velPermitida} km/h\n
-        Tera que paga uma muta de R$ 50,00"
-elsif (calMulta >= 11 && calMulta <= 30)
-    puts "Você estava na velocidade #{velMoto} Km/h em uma velocidade permitida na Avenida #{velPermitida} km/h\n
-    Tera que paga uma muta de R$ 100,00"
-elsif (calMulta >= 31)
-    puts "Você estava na velocidade #{velMoto} Km/h em uma velocidade permitida na Avenida #{velPermitida} km/h\n
-    Tera que paga uma muta de R$ 200,00"
-else 
-    puts "O motorista está conforme as leis"
-end
-
-# Solicita um resposta com sim ou não
-puts "\nDeseja continuar? (S/N)"
-# Armazena a resposta e converte para maiuscula, caso seja minuscula a resposta
-resposta = gets.chomp.upcase
-
-# Interrompe a loop caso a resposta for não
-break if resposta == "N"
-end
-
-# Apos encerrar o loop, mostra a mensagem 
-puts "\nOkay. Programa Encerrado!"
+    puts "Qual a velocidade permitida na avenida: "
+    velPermitida = gets.chomp.to_i
+    
+    puts "Qual a velocidade do motorista: "
+    velMoto = gets.chomp.to_i
+    
+    calMulta = velMoto - velPermitida 
+    
+    if(calMulta > 0 && calMulta <= 10)
+        puts "O motorista estava na velocidade #{velMoto} Km/h em uma velocidade permitida na Avenida #{velPermitida} km/h.
+        Tendo uma diferença de #{calMulta} Km/h. Tera que paga uma muta de R$ 50,00"
+    elsif (calMulta >= 11 && calMulta <= 30)
+        puts "O motorista estava na velocidade #{velMoto} Km/h em uma velocidade permitida na Avenida #{velPermitida} km/h
+        Tendo uma diferença de #{calMulta} Km/h. Tera que paga uma muta de R$ 100,00"
+    elsif (calMulta >= 31)
+        puts "O motorista estava na velocidade #{velMoto} Km/h em uma velocidade permitida na Avenida #{velPermitida} km/h
+        Tendo uma diferença de #{calMulta} Km/h. Tera que paga uma muta de R$ 200,00"
+    else 
+        puts "O motorista está conforme as leis"
+    end
+    
+    # Solicita um resposta com sim ou não
+    puts "\nDeseja continuar? (S/N)"
+    # Armazena a resposta e converte para maiuscula, caso seja minuscula a resposta
+    resposta = gets.chomp.upcase
+    
+    # Interrompe a loop caso a resposta for não
+    break if resposta == "N"
+    end
+    
+    # Apos encerrar o loop, mostra a mensagem 
+    puts "\nOkay. Programa Encerrado!"
