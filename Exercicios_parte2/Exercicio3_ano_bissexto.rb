04fe69e8218297e894dc42b93d79c3a6e48e4ab7
Gabrielle Barreto <105834363+GabsRB@users.noreply.github.com>
2024-07-05 14:15:40 -0300

    Add files via upload

--- Exercicios_parte2/Exercicio3_ano_bissexto.rb
+++ Exercicios_parte2/Exercicio3_ano_bissexto.rb
@@ -3,11 +3,30 @@
  é ou não bissexto. Dica: um ano é bissexto se ele for divisível por 400, ou se ele for divisível por 4 e não por 100.
 =end
 
-puts "Digite um ano"
-ano = gets.chomp.to_i
+# Inicia com um loop infinito para continuar com outros valores, que só será encerrado com a condição de break
+loop do
+    # Soicita que digite um ano
+    puts "Digite um ano"
+    # Lê a entrada e converte para um número inteiro
+    ano = gets.chomp.to_i
 
-if (ano/400 == 0) and (ano/100 == 0)
-    puts "O ano #{ano} é bissexto"
-elsif (ano/100 != 0 )
-    puts "O ano #{ano} não é bissexto"
+    # Verifica se o ano é divisível por 400 OU é divisível por 4 e não por 100
+    if (ano % 400 == 0) || (ano % 4 == 0 && ano % 100 != 0)
+        # Se for verdadeira, exibir mensagem que o ano é bissexto
+        puts "O ano #{ano} é bissexto"
+    else
+        # Caso não seja a primeira condição, exibir mensagem indicando que o ano não é bissexto
+        puts "O ano #{ano} não é bissexto"
+    end
+
+    # Solicita um resposta com sim ou não
+    puts "\nDeseja continuar? (S/N)"
+    # Armazena a resposta e converte para maiuscula, caso seja minuscula a resposta
+    resposta = gets.chomp.upcase
+
+    # Interrompe a loop caso a resposta for não
+    break if resposta == "N"
 end
+
+# Apos encerrar o loop, mostra a mensagem 
+puts "\nOkay. Programa Encerrado!"
